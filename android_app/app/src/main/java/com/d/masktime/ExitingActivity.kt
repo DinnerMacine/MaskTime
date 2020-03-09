8c055184102fce853edafda7082f24822c44d297
Midan2047 <jeff112630@gmail.com>
2020-03-09 18:34:37 +0900

    To merge

--- android_app/app/src/main/java/com/d/masktime/ExitingActivity.kt
+++ android_app/app/src/main/java/com/d/masktime/ExitingActivity.kt
@@ -1,7 +1,19 @@
 package com.d.masktime
 
 import android.app.Activity
+import android.util.Log
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ExitingActivity : Activity() {
+    var time = System.currentTimeMillis()
+    var mDate = Date(time)
+    fun gettime(){
+        time = System.currentTimeMillis()
+        mDate = Date(time)
+        var simpleDate = SimpleDateFormat("yyyy/MM/dd,hh:mm:ss")
+        var time : String = simpleDate.format(mDate)
+        var rDate = simpleDate.parse(time)
 
+    }
 }
