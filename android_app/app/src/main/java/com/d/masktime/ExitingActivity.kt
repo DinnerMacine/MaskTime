0fae9f6b4a63bfb5c073aa91712c4c9f84a2154f
Midan2047 <jeff112630@gmail.com>
2020-03-10 15:25:11 +0900

    half

--- android_app/app/src/main/java/com/d/masktime/ExitingActivity.kt
+++ android_app/app/src/main/java/com/d/masktime/ExitingActivity.kt
@@ -1,24 +1,50 @@
 package com.d.masktime
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ExitingActivity : Activity() {
     var time = System.currentTimeMillis()
     var mDate = Date(time)
-    fun gettime(){
+    val simpleDate = SimpleDateFormat("yyyy/MM/dd,hh:mm:ss")
+    var settingtime = simpleDate.format(mDate)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.exiting_layout)
+
+        var button = findViewById<Button>(R.id.Exitbutton)
+        button.setOnClickListener{
+            getCurrentTime()
+            setTime("time",settingtime)
+            val next = Intent(applicationContext, OutdoorActivity::class.java)
+            startActivity(next)
+        }
+    }
+
+    fun getCurrentTime() {
         time = System.currentTimeMillis()
         mDate = Date(time)
-        var simpleDate = SimpleDateFormat("yyyy/MM/dd,hh:mm:ss")
-        var time : String = simpleDate.format(mDate)
-        var rDate = simpleDate.parse(time)
-
-<<<<<<< HEAD
+        settingtime = simpleDate.format(mDate)
     }
-=======
 
+    fun setTime(key: String, value: String) {
+        val pref = getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val edit = pref.edit()
+        edit.putString(key, value)
+        edit.apply()
+    }
 
->>>>>>> 76d05021c199498aaa5eb416a5098a58d2181648
+    //값 불러오는 함수
+    fun getTime(key: String, value: String) : String? {
+        val pref = getSharedPreferences("pref", Context.MODE_PRIVATE)
+        return pref.getString(key, value)
+    }
 }
--- android_app/app/src/main/java/com/d/masktime/OutdoorActivity.kt
+++ android_app/app/src/main/java/com/d/masktime/OutdoorActivity.kt
@@ -2,20 +2,59 @@ package com.d.masktime
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import kotlinx.android.synthetic.main.outdoor_layout.*
+import android.util.Log
+import android.widget.Button
+import java.text.SimpleDateFormat
 import java.util.*
 
 class OutdoorActivity : Activity() {
+    var currentTime = System.currentTimeMillis()
+    var mDate = Date(currentTime)
+    val simpleDate = SimpleDateFormat("yyyy/MM/dd,hh:mm:ss")
+    var ScompareMe = simpleDate.format(mDate)
+
+    lateinit var tOut :String
+    var total :Long = 0L
+    lateinit var string  : String
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.outdoor_layout)
+        var button = findViewById<Button>(R.id.TimeButton)
+        val test = getTime("total","")
+        Log.v("test","$test")
+        if(test != "" && test != null){
+            val hNm = test.split(":")
+            total = hNm[0].toLong() * (60 * 60 * 1000) + hNm[1].toLong() * ( 60 * 1000)
+        }
+        val test2 = getTime("time","")
+        tOut = if(test2 == "" || test == null){
+            Log.v("error","무언가 잘못되엇습니다!")
+            ScompareMe
+        } else{
+            test2!!
+        }
 
-        setTime("time", "2020/03/09,18:22:22")
+        Log.v("total","" + total)
+        Log.v("out","" + simpleDate.parse(tOut))
 
-        val setMe = getTime("time", "")
 
-        textView.setText(setMe)
+        val compareMe1 = simpleDate.parse(ScompareMe)
+        val compareMe2 = simpleDate.parse(tOut)
+        var used = total + compareMe1.time - compareMe2.time
+        val h = used / (60  * 60 * 1000)
+        val m = (used / (60 * 1000))% 60
+        string = "$h:$m"
+        button.setOnClickListener{
+            calculateTime()
+            Log.v("saved",""+string)
+            setTime("total",string)
+            val next = Intent(this,ExitingActivity::class.java)
+            startActivity(next)
+        }
+        button.text = string
     }
 
     //값 저장하는 함수
@@ -30,5 +69,13 @@ class OutdoorActivity : Activity() {
     fun getTime(key: String, value: String) : String? {
         val pref = getSharedPreferences("pref", Context.MODE_PRIVATE)
         return pref.getString(key, value)
+    }
+    fun calculateTime(){
+        val compareMe1 = simpleDate.parse(ScompareMe)
+        val compareMe2 = simpleDate.parse(tOut)
+        var used = total + compareMe1.time - compareMe2.time
+        val h = used / (60  * 60 * 1000)
+        val m = (used / (60 * 1000)) % 60
+        string = "$h:$m"
     }
 }
